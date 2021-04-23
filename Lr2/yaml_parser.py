b91db563aca0235af47f9614af43338ca5373c3a
AshRaven521 <ivan.kondrashov.92@mail.ru>
2021-04-23 21:32:22 +0300

    nedeed function added

--- Lr2/yaml_parser.py
+++ Lr2/yaml_parser.py
@@ -1,7 +1,35 @@
 from my_parser import MyParser
 import yaml
+import inspect
 
 class YamlParser(MyParser):
+
+    def function_to_dictionary(self, func):
+        struct = {'__type__': 'function'}
+        args = []
+        if str(func).__contains__('lambda'):
+            s = str(inspect.getsource(func)[inspect.getsource(func).find("lambda"):])
+            struct['code'] = s
+            return struct
+        elif func.__name__.startswith('__'):
+            struct['name'] = func.__name__
+            args = inspect.getfullargspec(func).args
+            struct['args'] = args
+            return struct
+        else:
+            globs = {}
+            name = func.__name__
+            func_code = inspect.getsource(func)
+            if func_code.__contains__('global'):
+                subs = func_code.split('global')
+                for i in range(1, len(subs)):
+                    glob_key = subs[i].lstrip().split("\n")[0]
+                    globs.update(dict([(glob_key, func.__globals__[glob_key])]))
+
+            struct['name'] = name
+            struct['globals'] = globs
+            struct['code'] = func_code
+            return struct
 
     def dump(self, save_file_path,obj):
         with open(save_file_path, "w") as file:
