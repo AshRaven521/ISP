7238a8ebad5022b136b5e023ebcb2e7e7876e758
AshRaven521 <ivan.kondrashov.92@mail.ru>
2021-05-14 22:51:14 +0300

    some additions to custom json

--- Lr2/json_parser.py
+++ Lr2/json_parser.py
@@ -1,4 +1,6 @@
 import json
+import re
+
 from my_parser import Parser
 
 
@@ -57,3 +59,65 @@ class JsonParser(Parser):
 
     def loads(self, json_string):
         return super().loads(json.loads(json_string))
+
+    #def loads(self, json_string):
+    #    if 'code' in json_string:
+    #        return super().loads(self.json_to_function(json_string));
+    #    else:
+    #        return None
+
+    tuples = ['co_consts', 'co_names', 'co_varnames', 'co_freevars', 'co_cellvars']
+    lists = ['nlocals', 'co_flags', 'co_code']
+
+    def json_to_function(self, json_string):
+        json_string = json_string.replace("},\n\"globals\"", "}\n\"globals\"")
+        string = re.sub('[{}\"]', '', json_string)
+        arr = string.split('\n')
+        code = dict()
+        glob = dict()
+        arr = [item for item in arr if not item == '']
+        for item in arr:
+            pair = item.split(': ')
+            pair[1] = re.sub('[,]', '', pair[1])
+            if pair[0] == 'code' or pair[0] == 'globals':
+                continue
+            if pair[0] in self.tuples:
+                value = self.string_to_tuple(pair[1])
+            elif pair[0] in self.lists:
+                value = self.string_to_list(pair[1])
+            else:
+                if isinstance(pair[1], str):
+                    if pair[1].isdigit():
+                        value = int(pair[1])
+                    else:
+                        value = pair[1]
+
+            if pair[0].startswith('co_'):
+                code[pair[0]] = value
+            else:
+                glob[pair[0]] = value
+        func = dict()
+        func['code'] = code
+        func['globals'] = globals
+        return func
+
+    def string_to_tuple(self, string):
+        return self.string_to_collection(string, tuple())
+
+    def string_to_list(self, string):
+        return self.string_to_collection(string, list())
+
+    def string_to_collection(self, string, collection):
+        string = string.replace('[', '').replace(']', '')
+        arr = string.split(' ')
+
+        for item in arr:
+            if isinstance(item, str):
+                if item.isdigit():
+                    value = int(item)
+                    collection.add(value)
+                else:
+                    value = item
+                    collection.add(value)
+
+        return collection
