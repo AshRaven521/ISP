7d74519c364c2dbc8f67ce1f2ad5d2870dd2b5c3
AshRaven521 <ivan.kondrashov.92@mail.ru>
2021-05-15 11:46:29 +0300

    added fixes in custom loads

--- Lr2/json_parser.py
+++ Lr2/json_parser.py
@@ -1,73 +1,70 @@
 import json
-import re
-
 from my_parser import Parser
+import re
 
 
 class JsonParser(Parser):
 
     def dumps(self, obj):
         obj = super().dumps(obj)
-        temp_str = self.dumps_wrapper(obj)
-        temp_str = temp_str.replace("None","null")
-        temp_str = temp_str.replace("\'", "\"")
-        temp_str = temp_str.replace("\\", "\\\\")
-        return temp_str
-        #return json.dumps(super().dumps(obj), indent=4)
+        return self.dumps_wrapper(obj)
+        # return json.dumps(super().dumps(obj), indent=4)
 
     def dumps_wrapper(self, obj):
         json_string = "{\n"
         for key in obj:
             if isinstance(obj[key], dict):
                 temp_string = self.dumps_wrapper(obj[key])
-                json_string += "\"" + key + "\"" + ": " + temp_string + ", \n"
+                if key is list(obj.keys())[-1]:
+                    end = "\n"
+                else:
+                    end = ",\n"
+                json_string += "\"" + key + "\"" + ": " + temp_string + end
                 continue
-            elif isinstance(obj[key], list):
-                json_string += "\"" + key + "\"" + ": ["
 
+            elif isinstance(obj[key], list) or isinstance(obj[key], tuple):
+                json_string += "\"" + key + "\"" + ": ["
                 for i in range(len(obj[key])):
                     if isinstance(obj[key][i], str):
-                        json_string += "\"" + obj[key][i] + "\","
-                    else:
-                        list_str = str(obj[key][i])
-                        json_string += "\n" + list_str + ","
-                if json_string.endswith(","):
-                    json_string = json_string[: len(json_string) - 1] + "\n],\n"
+                        json_string += "\"" + obj[key][i] + "\", "
+                    elif isinstance(obj[key][i], int):
+                        json_string += str(obj[key][i]) + ", "
+                if json_string.endswith(", "):
+                    json_string = json_string[: len(json_string) - 2] + "],\n"
                 else:
-                    json_string += "\n],\n"
-            else:
-                if isinstance(obj[key], str):
-                    json_string += "\"" + key + "\"" + ": " + "\"" + obj[key] + "\"" + ", \n"
-                else:
-                    int_str = str(obj[key])
-                    if int_str == "()":
-                        int_str = "[]"
-                    if obj[key] is None:
-                        int_str = "[\nnull\n]"
-                    if int_str == "(None,)":
-                        int_str = "[\nnull\n]"
-                    if int_str.startswith("("):
-                        if int_str.endswith(")"):
-                            int_str = "[" + int_str[1:]
-                            int_str = int_str[:len(int_str) - 1] + "]"
-                    json_string += "\"" + key + "\"" + ": " + int_str + ", \n"
-        if json_string.endswith(", \n"):
-            json_string = json_string[: len(json_string) - 3] + "\n}"
+                    json_string += "],\n"
+
+            elif isinstance(obj[key], str):
+                json_string += "\"" + key + "\"" + ": " + "\"" + obj[key].replace('\\', '\\\\') + "\"" + ",\n"
+            elif isinstance(obj[key], int):
+                json_string += "\"" + key + "\"" + ": " + str(obj[key]) + ",\n"
+
+            elif isinstance(obj[key], type(None)):
+                json_string += "\"" + key + "\"" + ": " + 'null' + ",\n"
+
+            if key is list(obj.keys())[-1]:
+                json_string = json_string[: len(json_string) - 2]
+                json_string += '\n'
+
+        if json_string.endswith(",\n"):
+            json_string = json_string[:(len(json_string) - 2)] + "}"
         else:
             json_string += "}"
         return json_string
 
-    def loads(self, json_string):
-        return super().loads(json.loads(json_string))
 
     #def loads(self, json_string):
-    #    if 'code' in json_string:
-    #        return super().loads(self.json_to_function(json_string));
-    #    else:
-    #        return None
+    #    return super().loads(json.loads(json_string))
 
-    tuples = ['co_consts', 'co_names', 'co_varnames', 'co_freevars', 'co_cellvars']
-    lists = ['nlocals', 'co_flags', 'co_code']
+
+    def loads(self, json_string):
+        if 'code' in json_string:
+            return super().loads(self.json_to_function(json_string));
+        else:
+            return None
+
+    tuples = ['co_consts', 'co_names', 'co_freevars', 'co_cellvars']
+    lists = ['nlocals', 'co_code', 'co_lnotab', 'co_varnames']
 
     def json_to_function(self, json_string):
         json_string = json_string.replace("},\n\"globals\"", "}\n\"globals\"")
@@ -98,7 +95,7 @@ class JsonParser(Parser):
                 glob[pair[0]] = value
         func = dict()
         func['code'] = code
-        func['globals'] = globals
+        func['globals'] = glob
         return func
 
     def string_to_tuple(self, string):
@@ -110,14 +107,14 @@ class JsonParser(Parser):
     def string_to_collection(self, string, collection):
         string = string.replace('[', '').replace(']', '')
         arr = string.split(' ')
-
+        arr_val = []
         for item in arr:
             if isinstance(item, str):
                 if item.isdigit():
                     value = int(item)
-                    collection.add(value)
+                    arr_val.append(value)
                 else:
                     value = item
-                    collection.add(value)
-
-        return collection
+                    if value != '':
+                        arr_val.append(value)
+        return arr_val
