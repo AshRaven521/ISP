5ba8b86f5c553b988cadde2f33f0434487541f34
AshRaven521 <ivan.kondrashov.92@mail.ru>
2021-06-01 00:08:25 +0300

    change name

--- Lr3/app.py
+++
@@ -1,9 +0,0 @@
-from flask_migrate import Migrate
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-database = SQLAlchemy(app)
-migration = Migrate(app, database)
