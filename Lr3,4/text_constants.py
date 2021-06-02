d358678931393e78f1d7d191c87a700a734e0bf9
AshRaven521 <ivan.kondrashov.92@mail.ru>
2021-06-02 20:55:52 +0300

    fixed start message

--- Lr3,4/text_constants.py
+++ Lr3,4/text_constants.py
@@ -5,4 +5,5 @@ START_MESSAGE = 'Hello!\n' \
                 '2. Give link to a youtube video\n' \
                 '3. Wait\n' \
                 'If video larger than 49 MB you will get two or more video files(parts of one large video)\n' \
-                'Else you will get one video file.'
+                'Else you will get one video file\n\n' \
+                "Please, enter '/reg', if you here the first time"
