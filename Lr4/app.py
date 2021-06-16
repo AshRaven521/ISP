6d714894654734166cbd9399d49342904f6868a1
AshRaven521 <ivan.kondrashov.92@mail.ru>
2021-06-17 01:25:32 +0300

    fixed asynchronius functions

--- Lr4/app.py
+++ Lr4/app.py
@@ -6,10 +6,4 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 database = SQLAlchemy(app)
-#database.create_all()
 migration = Migrate(app, database)
-#database.create_all()
-
-#if __name__ == '__main__':
-    #database.create_all()
-    #app.run()
